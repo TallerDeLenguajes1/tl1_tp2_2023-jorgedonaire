49e1c6f120ccfb4ac796860b21f1f45ee6882605
Jorge Donaire <jorgedonaire93@gmail.com>
2023-05-03 15:16:58 -0300

    Agregue nueva version del punto 2

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -5,10 +5,13 @@ int main(){
     int i;
     double vt[N], *p;
     p = vt;
-    for(i = 0;i<N; i++)
+    for(i = 0;i<N; i++) //seria un while hasta N ya que esta incrementando p++
     {
         *p = 1+rand()%100;
         printf("%f ", *p);
         p++;
     }
+
+    getchar();
+    return 0;
 }
