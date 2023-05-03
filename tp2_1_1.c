6bba7b67ceaa7480a668b43bd1e9b7eb09c6f477
Jorge Donaire <jorgedonaire93@gmail.com>
2023-05-03 14:57:10 -0300

    Correccion de punto 2

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 20
 
 int main(){
+    srand(time(NULL));
     int i;
     double vt[N];
     for(i = 0;i<N; i++)
@@ -9,4 +12,7 @@ int main(){
         vt[i]=1+rand()%100;
         printf("%f ", vt[i]);
     }
+    
+    getchar();
+    return 0;
 }
